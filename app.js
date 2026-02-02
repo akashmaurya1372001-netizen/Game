fb9c754e0b1dddde4c919621efea9e2b0d02fb3e
akashmaurya1372001 <akashmaurya1372001@gmail.com>
2026-02-02 23:02:37 +0530

    working with javaScript

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 let userScore=0;
 let compScore=0;
-const choice=document.qur
+const choice=document.querySelectorAll(".choice");
+const genComputerChoice=()=>{
+
+}
+const playGame =(you) =>{
+    console.log("user choice=",userChoice);
+}
+choice.forEach((choice)=>{
+    choice.addEventListener("click",()=>{
+const userChoice =choice.getAttribute("id");
+playGame(userChoice);
+    })
+})
