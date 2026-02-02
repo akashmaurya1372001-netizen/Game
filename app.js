414f764d7e718918812a7d89783946b3cbc9b522
akashmaurya1372001-netizen <akashmaurya1372001@gmail.com>
2026-02-03 00:19:49 +0530

    working in javascript

--- app.js
+++ app.js
@@ -2,11 +2,16 @@ let userScore=0;
 let compScore=0;
 const choice=document.querySelectorAll(".choice");
 const genComputerChoice=()=>{
-
-}
+    const option =["rock","paper","scissor"];
+const randomidx=Math.floor(Math.random()*3);
+return option[randomidx];
+};
 const playGame =(you) =>{
     console.log("user choice=",userChoice);
-}
+    const campChoice =genComputerChoice();
+    console.log("comp choice=",campChoice);
+    if(userChoice==)
+    }
 choice.forEach((choice)=>{
     choice.addEventListener("click",()=>{
 const userChoice =choice.getAttribute("id");
