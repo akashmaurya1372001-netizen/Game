60c61979bf11b3976984a12850b77cf46287eb7a
akashmaurya1372001-netizen <akashmaurya1372001@gmail.com>
2026-02-03 01:08:59 +0530

    working with javascript

--- app.js
+++ app.js
@@ -1,16 +1,43 @@
 let userScore=0;
 let compScore=0;
 const choice=document.querySelectorAll(".choice");
+const msg =document.querySelector("#msg");
+
+const userScorepara=document.querySelector("#you");
+const compScorepara =document.querySelector("#camp");
+
 const genComputerChoice=()=>{
     const option =["rock","paper","scissor"];
 const randomidx=Math.floor(Math.random()*3);
 return option[randomidx];
 };
-const playGame =(you) =>{
-    console.log("user choice=",userChoice);
+const drawGame=()=>{
+    msg.innerText="Game was Draw. Play again.";
+    msg.style.background ="#081b31";
+};
+
+const showWinner =(userWin,userChoice,campChoice) =>{
+    if(userwin){
+        userScore++;
+        userScorepara.innerText =userScore;
+        msg.innerText=`You lose.${campChoice} beat your ${userChoice}`;
+        msg.style.backgroundcolor="red";
+    }
+};
+const playGame =(you) =>
+    {
     const campChoice =genComputerChoice();
     console.log("comp choice=",campChoice);
-    if(userChoice==)
+    if(userChoice== compChoice){
+        drawGame();
+     } else{
+            let userWin=true;
+            if(userChoice==="rock"){
+                userWin=campChoice==="paper" ?false:true;
+
+            }
+            elseif (userChoice==="paper")
+        }
     }
 choice.forEach((choice)=>{
     choice.addEventListener("click",()=>{
